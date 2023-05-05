791164bcf3be7e5aa449486adac0a68a66ce2809
daniloeberli <danilo.eberli@gmail.com>
2023-05-05 17:14:56 +0200

    feat: all Train data visualized

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,29 @@
         <title>Laravel</title>
          @vite('resources/js/app.js')
     </head>
-    <body>
+    <body class="p-3">
         <div class="container">
-            <h1>Prova</h1>
+            <h1 class="text-center">Treni</h1>
+            <!-- elemento card per visualizzare i dati in pagina -->
+            <div class="row">
+                @foreach ($trains as $train)
+                <div class="col col-sm-6 col-md-3 g-3">
+                    <div class="card text-center" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$train->azienda}}</h5>
+                            <h6 class="card-subtitle mb-2">{{$train->codice_treno}}</h6>
+                            <p class="card-text">{{$train->stazione_partenza}}</p>
+                            <p class="card-text">{{$train->stazione_arrivo}}</p>
+                            <p class="card-text">{{$train->is_orario}}</p>
+                            <p class="card-text">{{$train->is_cancellato}}</p>
+                            <p class="card-text">{{$train->numero_carrozze}}</p>
+                            <p class="card-text">{{$train->orario_partenza}}</p>
+                            <p class="card-text">{{$train->orario_arrivo}}</p>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>            
         </div>
 
     </body>
